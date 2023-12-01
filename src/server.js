b951e36b22fecda9f6d5eb8fa467daa10a246668
congmanh2k4 <nmanh2004@gmail.com>
2023-12-01 16:39:56 +0700

    init project -hi world

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
+require("dotenv").config();
+const app = express();
+
+//config view engine
+configViewEngine(app);
+
+//init web route
+initWebRoutes(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=> {
+    console.log(">> JWT Backend is running on the port = " + PORT);
+})
