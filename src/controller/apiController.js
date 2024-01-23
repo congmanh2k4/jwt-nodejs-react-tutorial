4fec1bdd8e49aad4eb86b1add85a51204299288e
congmanh2k4 <nmanh2004@gmail.com>
2024-01-23 23:21:05 +0700

    check permission of user

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -8,7 +8,7 @@ const testApi = (req,res) => {
 const handleLogin = async (req,res) => {
     try {
         let data = await loginRegister.handleUserLogin(req.body);
-        res.cookie("jwt", data.DT.access_token, {httpOnly: true});
+        res.cookie("jwt", data.DT.access_token, {httpOnly: true, maxAge: 60*60*1000 });
         return res.status(200).json({
             EM: data.EM,    
             EC: data.EC,
